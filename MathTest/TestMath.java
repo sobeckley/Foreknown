6c295d440c151ac8808d94da303465e47e5dc4a5
VigRamchandran <vramcha1@jhu.edu>
2014-09-06 19:45:33 -0400

    Mathv1.0

--- MathTest/TestMath.java
+++ MathTest/TestMath.java
@@ -20,6 +20,7 @@ public class TestMath {
 		for(int i = 0; i<arr.length - 1;i++){
 			lnSRatios[i] = Math.log(arr[i+1]/arr[i]);
 		}
+	
 
 		double sumRatios = 0;	
 		for(int i = 0; i<lnSRatios.length;i++){
@@ -27,26 +28,37 @@ public class TestMath {
 		}
 		
 	// Calculate U and S^2 which will be used to compute drift and volatility
-		double uBar = (1/lnSRatios.length)*sumRatios;
-		double sSquared = (1/(lnSRatios.length - 1))*(Math.pow(sumRatios - uBar, 2));
+		double uBar = (1.0/lnSRatios.length)*sumRatios;
+		double sSquared = (1.0/(lnSRatios.length - 1))*(Math.pow(sumRatios - uBar, 2));
+		
 		
 	//Stock drift and volatility
 		
-		int dt = 1;
-		int tFinal = 168;  // TODO change to reflect market hours
+		double dt = 0.5;
+		double tFinal = 32.5;  // TODO change to reflect market hours
 		double drift = Math.sqrt(sSquared)/Math.sqrt(dt);
 		double volatility = (uBar + sSquared/2)/dt;
 		double So = arr[arr.length - 1];
-	
+		double t = 0.0;
+		
 		Random r = new Random();
 		double Wt = 0;
-		double[] St = new double[tFinal/dt];
+		double[] St = new double[(int)(tFinal/dt)];
 		int counter = 0;
-		for (int t = 0; t < tFinal; t += dt) {
-			Wt = r.nextGaussian();
-			St[counter] = Math.exp(volatility*Wt + (drift - (Math.pow(volatility, 2))/2)*t);
-			So = St[counter];
+		
+		
+/*		for (int t = 0; t < tFinal; t += dt) {
+			Wt = r.nextGaussian()*Math.sqrt(dt);
+			St[counter] = So*Math.exp(volatility*Wt + (drift - (Math.pow(volatility, 2))/2)*t);
+			//So = St[counter];
 			counter++;
+		}*/
+		
+		while (counter < St.length  &&  t<= tFinal){
+			Wt = Math.log(Math.abs(r.nextGaussian()*Math.sqrt(dt)));
+			St[counter] = So*Math.exp(volatility*Wt + (drift - (Math.pow(volatility, 2))/2)*t);
+			counter ++;
+			t += dt;
 		}
 		
 		return St;
